9b42aeda47f4fee5892870d9a1e0900765e5a701
Erik Král <ekral@utb.cz>
2022-10-07 12:12:45 +0200

    DatabaseSevice VALUES fix

--- SharedProject/DatabaseService.cs
+++ SharedProject/DatabaseService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.Sqlite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,46 @@ using System.Threading.Tasks;
 
 namespace SharedProject
 {
-    internal class DatabaseService
+    public class DatabaseService
     {
+        private readonly string path = "hypoteka.db";
+
+        public void EnsureCreated()
+        {
+            if (System.IO.File.Exists(path))
+            {
+                using (var connection = new SqliteConnection($"Data Source={path}"))
+                {
+                    var command = connection.CreateCommand();
+                    command.CommandText = "CREATE TABLE Hypoteka (Id INTEGER PRIMARY KEY AUTOINCREMENT, LoanAmount DOUBLE, InterestRate DOUBLE, LoanTerm int)";
+                
+                    int count = command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Insert(Model model)
+        {
+            using (var connection = new SqliteConnection($"Data Source={path}"))
+            {
+                var command = connection.CreateCommand();
+                command.CommandText = $"INSERT INTO Hypoteka VALUES (@LoanAmount, @InterestRate, @LoanTerm)";
+                command.Parameters.Add($"@LoanAmount", SqliteType.Real).Value = model.LoanAmount;
+                command.Parameters.Add($"@InterestRate", SqliteType.Real).Value = model.InterestRate;
+                command.Parameters.Add($"@LoanTerm", SqliteType.Real).Value = model.LoanTerm;
+
+                int count = command.ExecuteNonQuery();
+            }
+        }
+
+        public void Update(Model model) 
+        {
+            throw new NotImplementedException();
+        }
+
+        public Model Get(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
